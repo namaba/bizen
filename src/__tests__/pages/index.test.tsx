4de3120d7ba87c566d7a5c3ea75eec9c72ae05b5
namaba <one.2426@gmail.com>
2021-11-07 20:33:26 +0900

    remove: 不要なテスト削除

--- src/__tests__/pages/index.test.tsx
+++ src/__tests__/pages/index.test.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Home from 'src/pages'
 import { render } from '../utils'
 
-test('ホームページ', () => {
-  const { container } = render(<Home />)
-  expect(container).toMatchSnapshot
-})
+// test('ホームページ', () => {
+//   const { container } = render(<Home />)
+//   expect(container).toMatchSnapshot
+// })
