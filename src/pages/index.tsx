c73abd16c4be13051547144b834fc0f46ee8cb39
namaba <one.2426@gmail.com>
2021-11-07 13:28:22 +0900

    feat: bizen-apiとのつなぎこみ

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,33 +7,55 @@ import { Header } from 'src/components/Header'
 import { Blog, BlogListGetResponse } from 'src/types/blog'
 import { client } from '../lib/client'
 
-export default function Home({ blogs }: { blogs: Blog[] }) {
+export default function Home(props: { hogehoge: string }) {
   return (
     <>
       <Header />
       <Box>
         <chakra.h1 color="tomato">Hello World</chakra.h1>
-        <ul>
-          {blogs.map((blog) => (
-            <li key={blog.id}>
-              <Link href={`/blog/${blog.id}`}>
-                <a>{blog.title}</a>
-              </Link>
-            </li>
-          ))}
-        </ul>
+        {/* {props} */}
+        {props.hogehoge}
       </Box>
       <Footer />
     </>
   )
 }
 
-export const getStaticProps: GetStaticProps = async () => {
-  const data = await client.get<BlogListGetResponse>({ endpoint: 'blog' })
+// microCMSのデータを使う場合
+// export default function Home({ blogs }: { blogs: Blog[] }) {
+//   return (
+//     <>
+//       <Header />
+//       <Box>
+//         <chakra.h1 color="tomato">Hello World</chakra.h1>
+//         <ul>
+//           {blogs.map((blog) => (
+//             <li key={blog.id}>
+//               <Link href={`/blog/${blog.id}`}>
+//                 <a>{blog.title}</a>
+//               </Link>
+//             </li>
+//           ))}
+//         </ul>
+//       </Box>
+//       <Footer />
+//     </>
+//   )
+// }
 
+export const getStaticProps: GetStaticProps<{ hogehoge: any }> = async () => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}`)
+  const data = await res.json()
   return {
     props: {
-      blogs: data.contents,
+      hogehoge: data.hogehoge,
     },
   }
+
+  // const data = await client.get<BlogListGetResponse>({ endpoint: 'blog' })
+  // return {
+  //   props: {
+  //     blogs: data.contents,
+  //   },
+  // }
 }
